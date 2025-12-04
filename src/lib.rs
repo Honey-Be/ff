462faf4ea99124f7829f54e33f53b3deb0046e1e
윤병익 <yeun0908@gmail.com>
2025-12-04 23:41:15 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -24,8 +24,6 @@ pub use bitvec::view::BitViewSized;
 
 #[cfg(feature = "bits")]
 use bitvec::{array::BitArray, order::Lsb0};
-
-use core::convert::Infallible;
 use core::fmt;
 use core::iter::{Product, Sum};
 use core::ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign};
@@ -77,14 +75,8 @@ pub trait Field:
 
     /// Returns an element chosen uniformly at random using a user-provided RNG.
     fn random<R: RngCore + ?Sized>(rng: &mut R) -> Self {
-        Self::try_from_rng(rng)
-            .map_err(|e: Infallible| e)
-            .expect("Infallible failed")
-
-        // NOTE: once MSRV gets to 1.82 remove the map_err/expect and use
-        // let Ok(out) = Self::try_from_rng(rng);
-        // out
-        // See: https://blog.rust-lang.org/2024/10/17/Rust-1.82.0.html#omitting-empty-types-in-pattern-matching
+        let Ok(out) = Self::try_from_rng(rng);
+        out
     }
 
     /// Returns an element chosen uniformly at random using a user-provided RNG.
